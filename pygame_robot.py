f22ba4cf3165e9f638bd5853f020ad675ed6db00
SavD <73773381+savateevdmit@users.noreply.github.com>
2024-01-18 13:02:07 +0300

    Add data base

--- pygame_robot.py
+++ pygame_robot.py
@@ -1,11 +1,12 @@
 import pygame
+import pymysql
 from PyQt5 import QtWidgets
 
 from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QLabel, QPushButton, QFrame, QTextEdit, QFileDialog, \
-    QMessageBox
-from PyQt5.QtCore import QTimer, QRect
+    QMessageBox, QMainWindow, QTableWidget, QDialog
+from PyQt5.QtCore import QTimer, QRect, QCoreApplication, QMetaObject
 from PyQt5.QtGui import QImage, QPainter, QPixmap, QColor
-
+from PyQt5.QtWidgets import QTableWidgetItem, QMessageBox
 from src.parser import parser
 
 class Constants:
@@ -13,6 +14,103 @@ class Constants:
     h = 900
     peach = (255, 228, 196)
 
+class Ui_Form(object):
+    def setupUi(self, Form):
+        if not Form.objectName():
+            Form.setObjectName(u"Form")
+        Form.resize(669, 385)
+        self.deleteFilmButton = QPushButton(Form)
+        self.deleteFilmButton.setObjectName(u"deleteFilmButton")
+        self.deleteFilmButton.setGeometry(QRect(10, 330, 651, 51))
+        self.filmsTable = QTableWidget(Form)
+        self.filmsTable.setObjectName(u"filmsTable")
+        self.filmsTable.setGeometry(QRect(10, 0, 651, 321))
+
+        self.retranslateUi(Form)
+
+        QMetaObject.connectSlotsByName(Form)
+    # setupUi
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        self.deleteFilmButton.setText(QCoreApplication.translate("Form", u"\u041e\u0442\u043a\u0440\u044b\u0442\u044c \u0444\u0430\u0439\u043b", None))
+    # retranslateUi
+
+class DB(QDialog, Ui_Form):  # Вот тут основное окно
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.up_f()
+        self.deleteFilmButton.clicked.connect(self.df)
+        self.dialogs = []
+        # self.exitAction.triggered.connect(self.exit)
+
+    def up_f(self):
+        result = []
+        connection = pymysql.connect(
+            host='185.221.213.34',
+            port=3306,
+            user='predprof',
+            password='Xz28]~w&V$weNQ%',
+            database='interpret',
+            cursorclass=pymysql.cursors.DictCursor
+        )
+
+        with connection.cursor() as cursor:
+            select_all_rows = "SELECT * FROM `data`"
+            cursor.execute(select_all_rows)
+            res = cursor.fetchall()
+        connection.close()
+        cursor.close()
+
+        for i in res:
+            res = list(i.values())
+            result.append(res[:-1])
+        print(result)
+
+        self.filmsTable.setRowCount(len(result))
+        self.filmsTable.setColumnCount(len(result[0]))
+        self.filmsTable.setHorizontalHeaderLabels(
+            ['Название файла', 'Дата сохранения'])
+
+        for i, elem in enumerate(result):
+            for j, val in enumerate(elem):
+                self.filmsTable.setItem(i, j, QTableWidgetItem(str(val)))
+
+    def df(self):
+        rows = list(set([i.row() for i in self.filmsTable.selectedItems()]))
+        ids = [self.filmsTable.item(i, 0).text() for i in rows]
+        if not ids:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Warning)
+            msg.setText("Выберите что-нибудь!")
+            msg.setWindowTitle("Уведомление")
+            msg.exec_()
+            return
+
+        else:
+            connection = pymysql.connect(
+                host='185.221.213.34',
+                port=3306,
+                user='predprof',
+                password='Xz28]~w&V$weNQ%',
+                database='interpret',
+                cursorclass=pymysql.cursors.DictCursor
+            )
+            print(int(''.join(ids)))
+
+            with connection.cursor() as cursor:
+                insert_query = f'''DELETE FROM `data` WHERE id = "{int(''.join(ids))}";'''
+                cursor.execute(insert_query)
+                connection.commit()
+                print('удалена')
+
+            connection.close()
+            cursor.close()
+            self.up_f()
+
+    def exit(self):
+        self.close()
 
 class Game():
     def __init__(self):
@@ -38,6 +136,7 @@ class Game():
         self.coords2 = []
 
         for i in self.coords1:
+            print(i)
             x = i[0]
             y = i[1]
             self.coords2.append([x * 28 + 300, -y * 28 + 300])
@@ -116,7 +215,7 @@ class Game():
         pygame.display.flip()
 
 
-class GameWidget(QWidget):
+class GameWidget(QMainWindow):
     def __init__(self, game=None, parent=None):
         super().__init__()
         # self.initUI()
@@ -194,13 +293,30 @@ class GameWidget(QWidget):
 
     def button_open_db_click(self):
         print("button_open_db")
-        # app = QApplication(sys.argv)
-        # form = Main()
-        # form.show()
-        # sys.exit(app.exec_())
+        self.window2 = DB()
+        self.window2.show()
 
     def button_save_db_click(self):
         print("button_save_db")
+
+        connection = pymysql.connect(
+            host='185.221.213.34',
+            port=3306,
+            user='predprof',
+            password='Xz28]~w&V$weNQ%',
+            database='interpret',
+            cursorclass=pymysql.cursors.DictCursor
+        )
+        a = self.textEdit1.toPlainText().strip()
+
+        with connection.cursor() as cursor:
+            insert_query = f'''INSERT INTO `data`(`file`, `time`, `txt`) VALUES ({'[value-1]'},{'[value-2]'},{'[value-3]'});'''
+            cursor.execute(insert_query)
+            connection.commit()
+            print('добавлено')
+
+        connection.close()
+        cursor.close()
 
     def button_start_click(self):
         print("button_start")
@@ -245,7 +361,7 @@ class GameWidget(QWidget):
 if __name__ == "__main__":
     import sys
 
-    app = QApplication(sys.argv)
+    app = QApplication(sys.argv[1:])
     w = GameWidget()
     w.resize(Constants.w - 50, Constants.h)
     w.show()
