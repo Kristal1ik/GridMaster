c302636eacb2d1639648a4cb657aa5e4c20c8523
Baritem <alexloza006@gmail.com>
2024-02-02 13:16:32 +0300

    Fixes

--- src/interpreter.py
+++ src/interpreter.py
@@ -78,31 +78,39 @@ class Interpreter:
         for idx, line in enumerate(lines):
             if line[0] == "PROCEDURE":
                 if len(stack) != 0:
-                    raise Exception(f"Объявление процедуры внутри блока {stack[-1]} на {idx}")
+                    raise Exception(f"Объявление процедуры внутри блока {stack[-1]} на {idx + 1}")
+                if line[1] in self.funcMap:
+                    raise Exception(f"Повторное объявление процедуры {line[1]} на {idx + 1}")
                 self.funcMap[line[1]] = idx
-                stack.append(("PROC", idx))
+                stack.append(("PROCEDURE", idx))
             if line[0] == "ENDPROC":
-                if stack[-1][0] != "PROC":
-                    raise Exception(f"Неожиданный ENDPROC на {idx}")
+                if stack[-1][0] != "PROCEDURE":
+                    raise Exception(f"Неожиданный ENDPROC на {idx + 1}")
                 stack.pop()
             if line[0] == "IFBLOCK":
-                stack.append(("IF", idx))
+                if len(stack) > 2:
+                    raise Exception(f"Слишком большая степень вложенности на {idx + 1}")
+                stack.append(("IFBLOCK", idx))
             if line[0] == "ENDIF":
-                if stack[-1][0] != "IF":
-                    raise Exception(f"Неожиданный ENDIF на {idx}")
+                if stack[-1][0] != "IFBLOCK":
+                    raise Exception(f"Неожиданный ENDIF на {idx + 1}")
                 self.ifMap[stack[-1][1]] = idx
                 stack.pop()
             if line[0] == "REPEAT":
+                if len(stack) > 2:
+                    raise Exception(f"Слишком большая степень вложенности на {idx + 1}")
                 stack.append(("REPEAT", idx))
             if line[0] == "ENDREPEAT":
                 if stack[-1][0] != "REPEAT":
-                    raise Exception(f"Неожиданный ENDREPEAT на {idx}")
+                    raise Exception(f"Неожиданный ENDREPEAT на {idx + 1}")
+                stack.pop()
+
 
             self.code.append([line[0], *line[1:]])
         self.code.append(["ENDPROG"])
         self.code.append(["ENDPROG"])
         if len(stack) != 0:
-            raise Exception(f"Незакрытый блок IF, PROCEDURE или REPEAT")
+            raise Exception(f"Незакрытый блок IF, PROCEDURE или REPEAT на {stack[-1][1]}")
         if __name__ == "__main__":
             print(self.code)
             print(self.ifMap)
@@ -124,6 +132,8 @@ class Interpreter:
                 if self.x < -10 or self.x > 10 or self.y < -10 or self.y > 10:
                     raise Exception("Исполнитель вышел за пределы поля!")
             case "IFBLOCK":
+                if len(self.stack) > 2:
+                    raise Exception(f"Слишком большая степень вложенности!")
                 if not Actions.checkIf(self, args[0]):
                     self.currentLine = self.ifMap[self.currentLine]
                     self.currentLine += 1
@@ -210,7 +220,7 @@ class Interpreter:
         if type(val) == int:
             return val
         if val not in self.variables:
-            raise Exception(f"Использование необъявленной переменной {val} на {self.currentLine}")
+            raise Exception(f"Использование необъявленной переменной {val} на {self.currentLine + 1}")
         return self.variables[val]
 
 
