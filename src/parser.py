f99e14c8a814f9ae7ba9be5a7a97830e2e3c6e99
Ipopuk <honenene@yandex.ru>
2024-02-02 13:15:34 +0300

    parser.py UPDATE

--- src/parser.py
+++ src/parser.py
@@ -3,14 +3,12 @@ from src.interpreter import Interpreter
 
 
 def parser(filename):
-    with open(filename, "r") as file:
-        file = file.read().splitlines()
-    commands = []
     delete_com = ["", "="]
+    with open(filename, "r") as file:
+        file = list(filter(lambda item: item != "", file.read().splitlines()))
+    commands = []
     for row in file:
         commands.append(list(filter(lambda com: com not in delete_com, (re.split(r'\s+', row)))))
-    interpreter = Interpreter()
-    interpreter.load(commands)
     interpreter = Interpreter()
     interpreter.load(commands)
     last = interpreter.next()
